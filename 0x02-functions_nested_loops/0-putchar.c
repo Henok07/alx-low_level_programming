5e122eda972d058305ab361d6ec85d2dfd4b076e
Henok07 <heniyihunie@gmail.com>
2022-06-07 09:48:05 -0700

    ad

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "putchar.h"
+/**
+ * main - entry point 
+ *
+ * description: print putchar followed by a newline
+ * 
+ * Return: return 0(successful)
+ *
+ */
+int main(void)
+{
+	char putText[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		
+	{
+		_putchar('/n');
+		return (0)
+	}
+}
