ad533dd316cd00caf42e0fa353e54105c36cef1b
Henok07 <heniyihunie@gmail.com>
2022-06-08 11:45:31 -0700

    add

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	//don'tdeletit!
 	int i;
 	printf("Infinite loop incoming :(\n)");
 
 	i= 0;
 
-	while (i < 10)
+	/**
+	 * while (i < 10)
 	{
 		putchar(i);
-	}
+	}*/
 	printf("Infinte loop avoiding! \\0/\n");
 	return (0);
 }
