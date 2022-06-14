0887f7cd51c8b874054488a5ef73980b1af9b86e
Henok07 <heniyihunie@gmail.com>
2022-06-13 19:30:15 -0700

    afdf

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ *reset_to_98 - takes a pointer to an integer,updates it to 98
  *
- * Return: Always 0.
+ *Return:void
+ *
+ *@n:input
+ *
  */
 void reset_to_98(int *n)
 {
